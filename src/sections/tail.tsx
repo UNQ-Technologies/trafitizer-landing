0feb46c8607e4c5bf28c47a5e6895af5da0ebba0
amjed-ali-k <amjedmgm@gmail.com>
2024-05-16 04:42:57 +0530

    fix: tail

--- src/sections/tail.tsx
+++ src/sections/tail.tsx
@@ -1,22 +1,23 @@
 import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
-import { LinkedinIcon } from "lucide-react";
+import { LinkedinIcon, Quote } from "lucide-react";
 
 function Tail() {
   return (
     <section className="bg-[#1a1a1a] text-white py-12 px-8">
-      <div className="max-w-6xl mx-auto flex justify-between items-center">
+      <div className="max-w-6xl mx-auto flex flex-col lg:flex-row justify-between items-center">
         <div className="space-y-6">
           <div>
-            <h3 className="text-4xl font-bold">TRAFITIZER</h3>
             <p className="text-2xl mr-8">
+            <Quote /> 
               It is more than just a product, it's a commitment to saving lives.
               Trafitizer deals with post crash emergency response time, it is a
               one of a kind technology that is proven to reduce 30% of emergency
               response delay time.
+          
             </p>
           </div>
         </div>
-        <div className="flex flex-col border p-4 rounded-lg border-gray-700 items-center">
+        <div className="flex flex-col mt-8 border p-4 rounded-lg border-gray-700 items-center">
           <Avatar className="rounded-sm w-72 h-auto">
             <AvatarImage
               alt="Mohamed Jasim"
